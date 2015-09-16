38eb9c4f4ace66a7b3f5013f5a8011689111c392
Yue Wu <wuyue681@gmail.com>
2015-09-16 01:40:14 +0100

    IntridiumSatellite and GeoSatellite. Add SatelliteAntenna in antenna.py.

--- core/antenna.py
+++ core/antenna.py
@@ -6,9 +6,13 @@ class Antenna(object):
     A class to decribe the antenna system
     """
     def __init__(self, f=0, gain=0, theta_3db=0, max_tp=0):
+        self.f = f
         self.gain = gain
         self.theta_3db = theta_3db
         self.max_tp = 0
+
+    def cal_gain(self):
+        return gain
 
 
 class ParabolicAntenna(Antenna):
@@ -21,5 +25,16 @@ class ParabolicAntenna(Antenna):
         """
         super(ParabolicAntenna, self).__init__(f, max_tp=max_tp)
         self.c = 3.0e8
+        self.d = d
         self.gain = (np.pi*d*f*1.0e9/self.c)**2 * ea
         self.theta_3db = (k*self.c/(f*1e9*d))/d
+
+    def cal_gain(self, alpha_t):
+        return self.gain - 0.00245*(alpha_t*self.d*self.f*1e9/self.c)**2
+
+
+class SatelliteAntenna(Antenna):
+    def __init__(self, intra_f, earth_f, gain, theta_3db, max_tp):
+        super(SatelliteAntenna, self).__init__(gain=gain, theta_3db=theta_3db, max_tp=max_tp)
+        self.intra_f = intra_f
+        self.earth_f = earth_f
--- satellite/satellite.py
+++ satellite/satellite.py
@@ -1,5 +1,6 @@
 import numpy as np
 from core.positions import cal_dist_3d
+from core.antenna import SatelliteAntenna, ParabolicAntenna
 
 
 class KeplerElement(object):
@@ -34,16 +35,16 @@ class Satellite(object):
         kes (list): array of Kepler elements of kes
         antennas (list): list of antennas
         stations (list): list of earth stations
-        init_pos (list): initial positions of satellites (in sphercial coordination system)
+        init_pos (numpy array): initial positions of satellites (in sphercial coordination system)
         .. note::
-        kes should have same shape as init_pos
+        kes, init_pos and antennas should have same number of rows.
         """
         self.kes = kes
         self.antennas = antennas
         self.stations = stations
         self.intra_bw = intra_bw
         self.earth_bw = earth_bw
-        self.init_pos = init_pos
+        self.pos = init_pos
 
     def add_satellite(self, kes, antennas):
         if self.kes is None:
@@ -57,7 +58,28 @@ class Satellite(object):
         self.stations.append(stations)
 
     def update_pos(self, t):
-        pass
+        if self.pos is None:
+            return
+        if type(kes) is not list:
+            self.pos = np.array([self.pos[0], self.pos[1]+self.kes.av*t, self.pos[2]])
+        self.pos = np.array([[p[0], p[1]+ke.av*t, p[2]]
+                             for p, ke in zip(self.pos, self.kes)])
+
+    def to_cartesian(self, idx=None):
+        if self.pos is None:
+            return None
+        if type(idx) is int:
+            p = self.pos
+            return np.array([self.p[0]*np.sin(self.p[1])*np.sin(self.p[2]),
+                             self.p[0]*np.sin(self.p[1])*np.cos(self.p[2]),
+                             self.p[0]*np.cos(self.p[1])])
+        if idx is None:
+            rpos = self.pos
+        else:
+            rpos = self.pos[idx, :]
+        return np.array([[p[0]*np.sin(p[1])*np.sin(p[2]),
+                          p[0]*np.sin(p[1])*np.cos(p[2]),
+                          p[0]*np.cos(p[1])] for p in rpos])
 
     def cal_satellite_topo(self):
         pass
@@ -71,5 +93,36 @@ class IridiumSatellite(Satellite):
         earth_bw (float): in MHz
         """
         super(IridiumSatellite, self).__init__(intra_bw=intra_bw, earth_bw=earth_bw, stations=stations)
+        self.kes = []
+        self.pos = []
+        self.antennas = []
+        for i in range(6):
+            for j in range(11):
+                self.kes.append(KeplerElement(0, 7136e3, np.pi/2, i*np.pi/6, 0, 0))
+                self.antennas.append(SatelliteAntenna(27.5, 6, 30, 0, 4.2))
+                if i % 2 == 0:
+                    self.pos.append([7136e3, 2*np.pi/11*j, i*np.pi/6])
+                else:
+                    self.pos.append([7136e3, np.pi/11+2*np.pi/11*j, i*np.pi/6])
+        self.pos = np.array(self.pos)
 
     def cal_satellite_topo(self):
+        topo = []
+        cpos = self.to_cartesian()
+        for i in range(66):
+            self.row = []
+            for j in range(66):
+                if i == j:
+                    self.row.append(np.inf)
+                if j == i-1 or j == i+1 or j == i+11 or j == i-11:
+                    self.row.append(cal_dist_3d(cpos[i, :], cpos[j, :]))
+            topo.append(self.row)
+        return np.array(topo)
+
+
+class GeoSatellite(Satellite):
+    def __init__(self, intra_bw=1, earth_bw=1, n=3, stations=None):
+        super(GeoSatellite, self).__init__(intra_bw=intra_bw, earth_bw=earth_bw, stations=stations)
+        self.kes = [KeplerElement(0, 42164e3, 0, 0, 0, 0) for i in range(n)]
+        self.pos = np.array([[42164e3, np.pi/2, i*2*np.pi/n] for i in range(n)])
+        self.antennas = [SatelliteAntenna(27.5, 6, 30, 0, 4.2) for i in range(n)]
