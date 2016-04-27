9177e5e94346069bcde08bd7d528e36d4d2cf083
Yue Wu <wuyue681@gmail.com>
2016-04-27 15:46:40 +0100

    add CASD.

--- main_sdsn.py
+++ main_sdsn.py
@@ -17,6 +17,7 @@ SDN_PACKET = 1500*8.0
 H_IP = 590*8.0
 COMM_THRESHOLD = 6e-18
 T_RETRY = 0.25
+CASD_SIG_DB = 5
 
 
 class LEOSystem(Satellite):
@@ -56,11 +57,14 @@ class SDSN(object):
         self.ue = LEOUE(user_loc)
         self.is_hard_success = False
         self.is_sdsn_success = False
+        self.is_casd_success = False
         self.is_hard_first = False
         self.is_sdsn_first = False
+        self.is_casd_first = False
         self.is_out_of_region = False
         self.sdsn_latency = 0
         self.hard_latency = 0
+        self.casd_latency = 0
         self.handover_t = 0
         self.region_t = t
         self.angle = np.arctan(user_loc/self.satellites['l'].height)
@@ -73,8 +77,7 @@ class SDSN(object):
         if self.satellites['l'].pos[0, 2] > 7*np.pi/12:
             return true
 
-    def handover_process(self):
-        # hard handover
+    def _hard_handover(self):
         throughput, rp = self.s_comm.comm_ue({'l': 1}, self.ue, self.angle, 1)
         # first try
         if rp > COMM_THRESHOLD:
@@ -90,7 +93,8 @@ class SDSN(object):
                 if rp > COMM_THRESHOLD:
                     self.is_hard_success = True
                     self._cal_hard_latency()
-        # sdsn handover
+
+    def _sdsn_handover(self):
         if not self.is_hard_first:
             self.satellites['l'].update_pos(-T_RETRY)
         # first try
@@ -124,15 +128,81 @@ class SDSN(object):
                     self.is_sdsn_success = True
                     # self._cal_hard_latency()
 
+    def _casd_handover(self):
+        # reset location
+        self.reset_leo()
+        self.begin_handover()
+        # check which handover scheme
+        throughput, rp = self.s_comm.comm_ue({'l': 0}, self.ue, self.angle, 1)
+        if rp > COMM_THRESHOLD * 10**(CASD_SIG_DB/10.0):
+            # hard handover
+            throughput, rp = self.s_comm.comm_ue({'l': 1}, self.ue, self.angle, 1)
+            # first try
+            if rp > COMM_THRESHOLD:
+                self.is_casd_success = True
+                self.is_casd_first = True
+                self._cal_casd_latency()
+                # second try
+            else:
+                if self.casd_latency + T_RETRY <= self.region_t:
+                    self.casd_latency = self.casd_latency + T_RETRY
+                    self.satellites['l'].update_pos(T_RETRY)
+                    throughput, rp = self.s_comm.comm_ue({'l': 1}, self.ue, self.angle, 1)
+                    if rp > COMM_THRESHOLD:
+                        self.is_casd_success = True
+                        self._cal_casd_latency()
+        else:
+            # soft one
+            # first try
+            throughput, rp = self.s_comm.comm_ue({'l': 1}, self.ue, self.angle, 1)
+            if rp > COMM_THRESHOLD:
+                self.is_casd_first = True
+                self.is_casd_success = True
+                self._cal_casd_latency()
+            else:
+                while True:
+                    throughput, rp = self.s_comm.comm_ue({'l': 0}, self.ue, self.angle, 1)
+                    if rp < COMM_THRESHOLD:
+                        break
+                    if self.casd_latency+T_RETRY > self.region_t:
+                        self.is_out_of_region = True
+                        break
+                    self.satellites['l'].update_pos(T_RETRY)
+                    # self.sdsn_latency = self.sdsn_latency + T_RETRY
+                    throughput, rp = self.s_comm.comm_ue({'l': 1}, self.ue, self.angle, 1)
+                    if rp > COMM_THRESHOLD:
+                        self.is_casd_first = True
+                        self.is_casd_success = True
+                        self._cal_sdsn_latency()
+                        break
+                # second try
+                if not self.is_casd_success and not self.is_out_of_region:
+                    self.casd_latency = self.casd_latency + T_RETRY
+                    self.satellites['l'].update_pos(T_RETRY)
+                    throughput, rp = self.s_comm.comm_ue({'l': 1}, self.ue, self.angle, 1)
+                    if rp > COMM_THRESHOLD:
+                        self.is_casd_success = True
+                        self._cal_casd_latency()
+
+    def handover_process(self):
+        # hard handover
+        self._hard_handover()
+        # sdsn handover
+        self._sdsn_handover()
+        # casd
+        self._casd_handover()
+
     # check handover status
     def get_handover_status(self):
         return self.is_hard_success,\
             self.is_hard_first,\
             self.is_sdsn_success,\
-            self.is_sdsn_first
+            self.is_sdsn_first,\
+            self.is_casd_success,\
+            self.is_casd_first
 
     def get_latency(self):
-        return self.hard_latency, self.sdsn_latency
+        return self.hard_latency, self.sdsn_latency, self.casd_latency
 
     def _cal_hard_latency(self):
         for i in range(2):
@@ -140,6 +210,14 @@ class SDSN(object):
             self.hard_latency = self.hard_latency + H_IP / throughput
             throughput, rp = self.s_comm.comm_ue({'l': 1}, self.ue, self.angle, 2)
             self.hard_latency = self.hard_latency + H_IP / throughput
+
+    def _cal_casd_latency(self):
+        for i in range(2):
+            throughput, rp = self.s_comm.comm_ue({'l': 1}, self.ue, self.angle, 1)
+            self.casd_latency = self.casd_latency + H_IP / throughput
+            throughput, rp = self.s_comm.comm_ue({'l': 1}, self.ue, self.angle, 2)
+            self.casd_latency = self.casd_latency + H_IP / throughput
+
 
     def _cal_sdsn_latency(self):
         # ue -> leo
@@ -169,20 +247,24 @@ class SDSN(object):
 def main():
     hard_failed_times = np.zeros([3, 8])
     sdsn_failed_times = np.zeros([3, 8])
+    casd_failed_times = np.zeros([3, 8])
     hard_first_times = np.zeros([3, 8])
     sdsn_first_times = np.zeros([3, 8])
+    casd_first_times = np.zeros([3, 8])
     hard_latency = np.zeros([3, 8])
     sdsn_latency = np.zeros([3, 8])
+    casd_latency = np.zeros([3, 8])
     i_hard = np.zeros([3, 8])
     i_sdsn = np.zeros([3, 8])
+    i_casd = np.zeros([3, 8])
     for k in range(3):
         for i in range(8):
             for j in range(LOOP):
                 sdsn_sim = SDSN(i, T_REGION[k], UE_LOC[k])
                 sdsn_sim.begin_handover()
                 sdsn_sim.handover_process()
-                h, hf, s, sf = sdsn_sim.get_handover_status()
-                hl, sl = sdsn_sim.get_latency()
+                h, hf, s, sf, c, cf = sdsn_sim.get_handover_status()
+                hl, sl, cl = sdsn_sim.get_latency()
                 if not h:
                     hard_failed_times[k, i] = hard_failed_times[k, i] + 1
                 else:
@@ -197,15 +279,26 @@ def main():
                     i_sdsn[k, i] = i_sdsn[k, i] + 1
                 if not sf:
                     sdsn_first_times[k, i] = sdsn_first_times[k, i] + 1
+                if not c:
+                    casd_failed_times[k, i] = casd_failed_times[k, i] + 1
+                else:
+                    casd_latency[k, i] = casd_latency[k, i] + cl
+                    i_casd[k, i] = i_casd[k, i] + 1
+                if not cf:
+                    casd_first_times[k, i] = casd_first_times[k, i] + 1
+
 
     hard_mean_latency = hard_latency / i_hard * 1000.0
     sdsn_mean_latency = sdsn_latency / i_sdsn * 1000.0
+    casd_mean_latency = casd_latency / i_casd * 1000.0
     hard_score = 1-np.exp(-1.0/hard_mean_latency)-0.1*hard_first_times/10000-0.1*hard_failed_times/10000
     sdsn_score = 1-np.exp(-1.0/sdsn_mean_latency)-0.1*sdsn_first_times/10000-0.1*sdsn_failed_times/10000
+    casd_score = 1-np.exp(-1.0/casd_mean_latency)-0.1*casd_first_times/10000-0.1*casd_failed_times/10000
 
     # 1000 1500 bytes
     sdsn_throughput = np.zeros([3, 8])
     hard_throughput = np.zeros([3, 8])
+    casd_throughput = np.zeros([3, 8])
     total_bits = 10000*1500*8
     for i in range(3):
         for j in range(8):
@@ -227,8 +320,14 @@ def main():
             t_sdsn = t +\
                      sdsn_failed_times[i, j]*0.75*n_handover/LOOP +\
                      (LOOP-sdsn_failed_times[i, j])*sdsn_mean_latency[i, j]/LOOP*n_handover
+            # casd
+            t_casd = t +\
+                     casd_failed_times[i, j]*0.75*n_handover/LOOP +\
+                     (LOOP-casd_failed_times[i, j])*casd_mean_latency[i, j]/LOOP*n_handover
+
             sdsn_throughput[i, j] = 10000*1500*8 / t_sdsn
             hard_throughput[i, j] = 10000*1500*8 / t_hard
+            casd_throughput[i, j] = 10000*1500*8 / t_casd
 
     with open('score.csv', 'wb') as f:
         dw = csv.writer(f, delimiter=' ')
@@ -236,41 +335,41 @@ def main():
             dw.writerow(hard_score[i, :])
         for i in range(3):
             dw.writerow(sdsn_score[i, :])
+        for i in range(3):
+            dw.writerow(casd_score[i, :])
     with open('latency.csv', 'wb') as f:
         dw = csv.writer(f, delimiter=' ')
         for i in range(3):
             dw.writerow(hard_mean_latency[i, :])
         for i in range(3):
             dw.writerow(sdsn_mean_latency[i, :])
+        for i in range(3):
+            dw.writerow(casd_mean_latency[i, :])
     with open('complete_failed.csv', 'wb') as f:
         dw = csv.writer(f, delimiter=' ')
         for i in range(3):
             dw.writerow(hard_failed_times[i, :])
         for i in range(3):
             dw.writerow(sdsn_failed_times[i, :])
+        for i in range(3):
+            dw.writerow(casd_failed_times[i, :])
     with open('first_failed.csv', 'wb') as f:
         dw = csv.writer(f, delimiter=' ')
         for i in range(3):
             dw.writerow(hard_first_times[i, :])
         for i in range(3):
             dw.writerow(sdsn_first_times[i, :])
+        for i in range(3):
+            dw.writerow(casd_first_times[i, :])
     with open('throughput.csv', 'wb') as f:
         dw = csv.writer(f, delimiter=' ')
         for i in range(3):
             dw.writerow(hard_throughput[i, :])
         for i in range(3):
             dw.writerow(sdsn_throughput[i, :])
+        for i in range(3):
+            dw.writerow(casd_throughput[i, :])
 
-    # x = np.linspace(160, 2000, 8)
-    # plt.plot(x, hard_mean_latency[0, :], '-o', linewidth=2, label='Hard, t=0')
-    # plt.plot(x, sdsn_mean_latency[0, :], '--o', linewidth=2, label='SDSN, t=0')
-    # plt.plot(x, hard_mean_latency[1, :], '-x', linewidth=2, label='Hard, t=250')
-    # plt.plot(x, sdsn_mean_latency[1, :], '--x', linewidth=2, label='SDSN, t=250')
-    # plt.plot(x, hard_mean_latency[2, :], '-^', linewidth=2, label='Hard, t=500')
-    # plt.plot(x, sdsn_mean_latency[2, :], '--^', linewidth=2, label='SDSN, t=500')
-    # plt.legend()
-    # plt.savefig('sdsn_latency.eps', format='eps')
-    # plt.show()
 
 if __name__ == '__main__':
     main()
